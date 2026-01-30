d8a465f5b9f191e71871dfea8bb7dd258f9873b2
devanshumankar <devanshumankar@gmail.com>
2026-01-30 11:04:37 +0530

    updated p

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
 	const [count, setCount] = useState(0);
 	return (
 		<div>
-			<p> Button clicked {count} times</p>
+			<p>Button clicked {count} times </p>
 			<button onClick={() => setCount((prev) => prev + 1)}>Click Me</button>
 			{/* Do not remove the main div */}
 		</div>
